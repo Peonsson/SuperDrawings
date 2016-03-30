2e4a3d2f2cb176875c34a5bdad6e36d1cc1c80ae
Johan Pettersson <xsnudelx@hotmail.com>
2016-03-30 18:52:59 +0200

    Created hello world!

---
+++ src/main/java/controller/TestApp.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Created by Peonsson on 2016-03-30.
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
