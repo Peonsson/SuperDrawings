eb30d21b551caf8cfcce306b9b0d04f6a0740a77
Robin <robinvet@kth.se>
2016-03-30 19:09:12 +0200

    Add preliminary GUI.

--- src/main/java/controller/TestApp.java
+++ src/main/java/controller/TestApp.java
@@ -1,5 +1,9 @@
 package controller;
 
+import view.MainView;
+
+import javax.swing.*;
+
 /**
  * Created by Peonsson on 2016-03-30.
  */
@@ -7,5 +11,11 @@ public class TestApp {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        JFrame frame = new JFrame("MainView");
+        frame.setContentPane(new MainView().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/main/java/view/MainView.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by Robin on 2016-03-30.
+ */
+public class MainView {
+    private JButton button1;
+    public JPanel mainPanel;
+}
