378b09942541de625eec2314c25da363a12a98a7
Peonsson <xsnudelx@hotmail.com>
2016-03-31 11:10:06 +0200

    alpha view

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -6,6 +6,12 @@ import javax.swing.*;
  * Created by Robin on 2016-03-30.
  */
 public class MainView {
-    private JButton button1;
+    private JButton selectButton;
     public JPanel mainPanel;
+    private JPanel top;
+    private JButton ovalButton;
+    private JButton rectangleButton;
+    private JButton squareButton;
+    private JButton undoButton;
+    private JButton redoButton;
 }
