8155cc67044435bc76e642cf5724f5c288afbc83
Johan Pettersson <xsnudelx@hotmail.com>
2016-04-02 15:20:29 +0200

    prettified

--- src/main/java/controller/TestApp.java
+++ src/main/java/controller/TestApp.java
@@ -10,9 +10,9 @@ import javax.swing.*;
 public class TestApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 
-        JFrame frame = new JFrame("MainView");
+        JFrame frame = new JFrame("Super Drawings");
         frame.setContentPane(new MainView().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
